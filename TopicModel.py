84d57a404bea5bdfb7ed499cf5971455f3e0ffc3
David Appel <ADavid@aureustechsystems.com>
2019-11-26 11:39:06 -0700

    undoing so i can focus on somethign else

--- TopicModel.py
+++ TopicModel.py
@@ -1,10 +1,13 @@
 import gensim
+import gensim.models.keyedvectors as word2vec
 
 # our classes
 import blobRepo
 import Db
 from PreprocessText import PlainTextPreprocessor
 
+model=word2vec.KeyedVectors.load_word2vec_format('GoogleNews-vectors-negative300.bin.gz', binary=True)  
+i2w = model.wv.index2word
 
 def identify_topics(num_topics=5, no_below=3, no_above=.34, passes=50):
     #create topics based on lemma lists created from whole files
@@ -44,6 +47,10 @@ def identify_topics(num_topics=5, no_below=3, no_above=.34, passes=50):
                                              )
     return(lda_model_tfidf, dictionary)
 
+#todo need to move this to factory so we can have custom models for bigrams
+def gettrained_bigram_model():
+    raw_list = list(filter(lambda s: '_' in s, i2w ))
+    return [tup for tup in raw_list if len(tup)> 0]
 
 if __name__ == "__main__":
     identify_topics(num_topics=20, no_above=.95, no_below=.25)
